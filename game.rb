3608472fd027fb97fdfd2a7da850f6b246e1f901
Isaac Lyons <drajonknives@gmail.com>
2020-06-21 04:06:12 +0000

    Clear the screen at the start of the game.

--- game.rb
+++ game.rb
@@ -2,28 +2,41 @@
 
 class Game
  	def self.keep_going?()
-                if (Player.list.first.score >= 10 || Player.list.last.score >= 10)
-                        return false
-                else
-                        return true
-                end
+        if (Player.list.first.score >= 5 || Player.list.last.score >= 5)
+            return false
+        else
+            return true
         end
+    end
+
+    def self.input_number(string)
+        1.times do
+            print string
+            input = gets.chomp
+            input if Float(input) rescue redo
+        end
+    end
 
 	def self.change_cards()
 		Player.dealer.set_vulnerable=(false)
 		Player.eldest_hand.set_vulnerable=(true)
 		first = true
+
 		while Player.eldest_hand.discard?()
 			Player.eldest_hand.set_vulnerable=(false)
 
-			print "How many cards would you, #{Player.eldest_hand.name}, like to discard? "
-			answer = gets.to_i
+            answer  = Game.input_number("How many cards would you, #{Player.eldest_hand.name}, like to discard? ")
+            puts
+
 			#this could be a method
 			if answer > Cards.deck.count
 				puts "There aren't enough cards for that. Choose a different number, please.\n"
 				redo
 			end
 			#
+
+            Player.dealer.hand
+            puts
 			if Player.dealer.permission?(answer)
 				first = false
 				Player.eldest_hand.redraw(answer)
@@ -43,5 +56,18 @@ class Game
 			puts "#{Player.eldest_hand.name} is vulnerable.\n"
 		end
 		puts
+
+        Player.list.each do |player|
+            player.hand.each do |card|
+                if card.number == "King" && card.suit == Cards.trump.suit
+                    puts "Do you wish to reveal the King of trumps?(Y/n)"
+                    answer = gets.chomp
+                    if answer == "y"
+                        puts card.name
+                        player.score += 1
+                    end
+                end
+            end
+        end
 	end
 end
